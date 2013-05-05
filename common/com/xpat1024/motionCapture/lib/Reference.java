39401aa2b9c9f04713b8648ca923a8591a65f550
Patrick Lowry <patrick.lowry@gmail.com>
2013-05-05 22:56:51 +0100

    First code uploaded

---
+++ common/com/xpat1024/motionCapture/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.xpat1024.motionCapture.lib;
+
+public class Reference {
+    public static final String MOD_ID = "mC";
+    public static final String MOD_NAME = "motionCapture";
+    public static final String VERSION = "0.0.1";
+
+}
---
+++ common/com/xpat1024/motionCapture/motionCapture.java
@@ -0,0 +1,34 @@
+package com.xpat1024.motionCapture;
+
+import com.xpat1024.motionCapture.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(
+        modid = Reference.MOD_ID, 
+        name = Reference.MOD_NAME, 
+        version = Reference.VERSION
+        )
+
+public class motionCapture {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+
+}
